d0bd004c5a7d4fa691195fff716ec852103fd43d
RomaGenZA <romagilda@yandex.ru>
2024-10-05 16:27:38 +0300

    Api Promise to async/await

--- src/components/base/api.ts
+++ src/components/base/api.ts
@@ -19,24 +19,31 @@ export class Api {
         };
     }
 
-    protected handleResponse(response: Response): Promise<object> {
-        if (response.ok) return response.json();
-        else return response.json()
-            .then(data => Promise.reject(data.error ?? response.statusText));
+    protected async handleResponse(response: Response): Promise<object> {
+        if (response.ok)
+            return response.json();
+        else {
+            const data = await response.json()
+            throw new Error(data.error ?? response.statusText)
+        }
     }
 
-    get(uri: string) {
-        return fetch(this.baseUrl + uri, {
+    async get(uri: string): Promise<object> {
+        const response = await fetch(this.baseUrl + uri, {
             ...this.options,
             method: 'GET'
-        }).then(this.handleResponse);
+        });
+        
+        return this.handleResponse(response);
     }
 
-    post(uri: string, data: object, method: ApiPostMethods = 'POST') {
-        return fetch(this.baseUrl + uri, {
+    async post(uri: string, data: object, method: ApiPostMethods = 'POST'): Promise<object> {
+        const response = await fetch(this.baseUrl + uri, {
             ...this.options,
             method,
             body: JSON.stringify(data)
-        }).then(this.handleResponse);
+        });
+        
+        return this.handleResponse(response);
     }
 }
